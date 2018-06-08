b27c4d4c8877cde60045a596ddd8b441448ecd2c
Badem <38181128+bademcurrency@users.noreply.github.com>
2018-06-08 18:10:44 +0300

    seems ok

--- docker/node/entry.sh
+++ docker/node/entry.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+PATH="${PATH:-/bin}:/usr/bin"
+export PATH
+
 set -euo pipefail
 IFS=$'\n\t'
 
-network="$(cat /etc/nano-network)"
+network="$(cat /etc/badem-network)"
 case "${network}" in
         live|'')
                 network='live'
@@ -17,11 +20,46 @@ case "${network}" in
                 ;;
 esac
 
-nanodir="${HOME}/RaiBlocks${dirSuffix}"
-mkdir -p "${nanodir}"
-if [ ! -f "${nanodir}/config.json" ]; then
+bademdir="${HOME}/Badem${dirSuffix}"
+dbFile="${bademdir}/data.ldb"
+mkdir -p "${bademdir}"
+if [ ! -f "${bademdir}/config.json" ]; then
         echo "Config File not found, adding default."
-        cp "/usr/share/raiblocks/config/${network}.json" "${nanodir}/config.json"
+        cp "/usr/share/badem/config/${network}.json" "${bademdir}/config.json"
 fi
 
-/usr/bin/rai_node --daemon
+pid=''
+firstTimeComplete=''
+while true; do
+	if [ -n "${firstTimeComplete}" ]; then
+		sleep 10
+	fi
+	firstTimeComplete='true'
+
+	if [ -f "${dbFile}" ]; then
+		dbFileSize="$(stat -c %s "${dbFile}" 2>/dev/null)"
+		if [ "${dbFileSize}" -gt $[1024 * 1024 * 1024 * 20] ]; then
+			echo "ERROR: Database size grew above 20GB (size = ${dbFileSize})" >&2
+
+			while [ -n "${pid}" ]; do
+				kill "${pid}" >/dev/null 2>/dev/null || :
+				if ! kill -0 "${pid}" >/dev/null 2>/dev/null; then
+					pid=''
+				fi
+			done
+
+			badem_node --vacuum
+		fi
+	fi
+
+	if [ -n "${pid}" ]; then
+		if ! kill -0 "${pid}" >/dev/null 2>/dev/null; then
+			pid=''
+		fi
+	fi
+
+	if [ -z "${pid}" ]; then
+		badem_node --daemon &
+		pid="$!"
+	fi
+done
